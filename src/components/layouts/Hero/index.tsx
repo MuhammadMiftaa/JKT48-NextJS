7fd18dbb66386e859c2ba6837c1c04ce7ce03555
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2024-08-05 10:15:12 +0700

    mini bug fixing

--- src/components/layouts/Hero/index.tsx
+++ src/components/layouts/Hero/index.tsx
@@ -19,7 +19,7 @@ export default function Hero() {
       className="h-screen overflow-hidden bg-no-repeat bg-center bg-cover flex items-end px-2 pb-8 animate__animated animate__fadeIn animate__slow after:content-[''] after:absolute after:-bottom-20 after:top-1/2 md:after:top-1/2 after:left-0 after:right-0 after:bg-gradient-to-t after:from-black after:to-transparent after:from-20% duration-1000"
       style={{ backgroundImage: `url("/image/hero/${heroImage}.jpg")` }}
     >
-      <div className="w-full z-10">
+      <div className="w-full z-10 mb-16 md:mb-0">
         <h1
           className="text-4xl md:text-6xl mb-2 font-bold text-custom-green font-poppins animate__animated animate__fadeInLeft  animate__delay-1s animate"
           style={{ textShadow: "3px 3px 5px #000" }}
@@ -27,7 +27,7 @@ export default function Hero() {
           JKT48
         </h1>
         <p
-          className="mix-blend-difference text-[10px] md:w-[60%] md:mb-10 md:text-sm font-extralight text-slate-300 tracking-[.1px] font-poppins animate__animated animate__fadeInLeft animate__delay-1s animate__slow flex gap-3"
+          className="mix-blend-difference text-[10px] md:w-[60%] md:mb-6 md:text-sm font-extralight text-slate-300 tracking-[.1px] font-poppins animate__animated animate__fadeInLeft animate__delay-1s animate__slow flex gap-3"
           style={{ textShadow: "1px 1px 5px #000" }}
         >
           <span className="tracking-[-2px]  text-custom-green">————</span>
