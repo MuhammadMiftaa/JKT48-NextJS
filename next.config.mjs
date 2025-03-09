ed5f34d69d7e0b579fc9d487de8825cd85f53e45
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2025-03-09 16:59:52 +0700

    create manifest and setup asset for pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
+import nextPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = nextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})({
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "assets.aceternity.com", "pbs.twimg.com", "jkt48.com", "res.cloudinary.com" ],
+    domains: [
+      "images.unsplash.com",
+      "assets.aceternity.com",
+      "pbs.twimg.com",
+      "jkt48.com",
+      "res.cloudinary.com",
+    ],
   },
-};
+});
 
 export default nextConfig;
